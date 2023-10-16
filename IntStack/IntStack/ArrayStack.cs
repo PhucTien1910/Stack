8f356e637db04e1a8eebc4d95064b68e4183d06a
Phúc Tiền Nguyễn <phuctien.nguyen2004@gmail.com>
2023-10-16 10:38:43 +0700

    mới tạo Stack

---
+++ IntStack/IntStack/ArrayStack.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntStack
+{
+    internal class ArrayStack
+    {
+        //Atribute
+        private int[] stkArray;
+        private int stkMax;
+        private int stkTop;
+
+        //Get/Set
+        public int[] StkArray
+        {
+            get { return stkArray; }
+            set { stkArray = value; }
+        }
+        public int StkMax
+        {
+            get { return stkMax; }
+            set { stkMax = value; }
+        }
+        public int StkTop
+        {
+            get { return stkTop; }
+            set { stkTop = value; }
+        }
+
+        //Constructor
+        public ArrayStack(int max = 0)
+        {
+            stkMax = max;
+            stkArray = new int[max];
+            stkTop = -1;
+        }       
+        public bool IsEmpty => stkTop == -1;
+        public bool IsFull => stkTop == stkMax - 1;
+        public bool Push(int x)
+        {
+            
+        }
+        public bool Pop(out int outItem)
+        {
+            throw new NotImplementedException();
+        }
+        public bool GetTop(out int outItem)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ IntStack/IntStack/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntStack
+{
+    internal class Program
+    {
+        static void Input(ArrayStack arrStack)
+        {
+            throw new NotImplementedException();
+        }
+        static void Output(ArrayStack arrStack)
+        {
+            throw new NotImplementedException();
+        }
+        static void TestArrayStack()
+        {
+            ArrayStack arrStack = new ArrayStack(1000);
+            Input(arrStack);
+            Console.Write("Cac gia tri trong Stack la: ");
+            Output(arrStack);
+        }
+        static void Main(string[] args)
+        {
+            TestArrayStack();
+            Console.ReadKey();
+        }
+    }
+}
